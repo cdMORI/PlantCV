8d2d151964c317d5bccd5bf6ebb8ff323adb0c0e
felixf2026 <124310762+felixf2026@users.noreply.github.com>
2025-03-27 15:00:29 -0700

    Update taking_photos.py
    make changes be more clear for debugging. fix the time issue.

--- taking_photos.py
+++ taking_photos.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 # Create the folder to store images (if it doesn't already exist)
 # Make sure to change the pathway depending on host computer
-folder_path = "/home/mori1/Desktop/Moriphotos"
+folder_path = "/home/mori2/Desktop/Moriphotos"
 if not os.path.exists(folder_path):
     os.makedirs(folder_path)
 
@@ -15,10 +15,11 @@ rounds = 5 # set the amount of rounds
 amount = 3 # set the amount of pics per round
 seconds = 60 # seconds between each round
 
-ctime = datetime.now().time()
-ctime = datetime.now().strftime("%H:%M:%S")
+
 for rds in range (0, rounds):
     for pic in range(0, amount):
+        ctime = datetime.now().time()
+        ctime = datetime.now().strftime("%H:%M:%S")
         # Make the filename for each image (H:M:S_Y-M-D.jpg)
         filename = os.path.join(folder_path, f"{ctime}_{datetime.today().date()}.jpg")
         
@@ -26,6 +27,6 @@ for rds in range (0, rounds):
         image = subprocess.run(["libcamera-still"," --width"," 800"," --height ","800","-o", filename])
 
     
-        print(f"Picture {rds +1 }.{pic + 1} taken. Waiting for 2 minutes")
+        print(f"Picture {rds +1 }.{pic + 1} taken. Waiting for 1 minute     filename:{ctime}_{datetime.today().date()}")
         time.sleep(1)
     time.sleep(seconds) # sleep after a round is done.
